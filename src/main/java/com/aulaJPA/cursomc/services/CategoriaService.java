2c079df79473124f7b5a4f6082b4d59018959bd0
Vinicius Yukio Ishikawa <ishikawavini@hotmail.com>
2023-02-27 15:36:18 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/aulaJPA/cursomc/services/CategoriaService.java
+++ src/main/java/com/aulaJPA/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.aulaJPA.cursomc.domain.Categoria;
+import com.aulaJPA.cursomc.dto.CategoriaDTO;
 import com.aulaJPA.cursomc.repositories.CategoriaRepository;
 import com.aulaJPA.cursomc.services.exceptions.DataIntegrityException;
 import com.aulaJPA.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 }
